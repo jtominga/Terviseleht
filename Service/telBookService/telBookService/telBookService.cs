66881cc936eaaf3f4d8907baddc7cd73e238733c
Firstname Lastname <joonas.puua@eesti.ee>
2012-04-24 17:01:41 +0300

    first commit

---
+++ Service/telBookService/telBookService/telBookService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+
+namespace telBookService
+{
+    [DataContract]
+    public class Contact
+    {
+        [DataMember]
+        public string submitter;
+        [DataMember]
+        public string tel;
+        [DataMember]
+        public string first;
+        [DataMember]
+        public string last;
+
+    }
+    [ServiceContract]
+    public interface ItelBookService
+    {
+        [OperationContract]
+        void submitkontakt(Contact kontakt);
+        [OperationContract]
+        List<Contact> getContacts();
+    }
+
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+    public class telBookService : ItelBookService
+    {
+        List<Contact> kontaktid = new List<Contact>();
+
+        public void submitkontakt(Contact kontakt)
+        {
+            kontaktid.Add(kontakt);
+        }
+
+        public List<Contact> getContacts()
+        {
+            return kontaktid;
+        }
+    }
+}
