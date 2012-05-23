600e244268027151393b8be1d301fbc5731b8d3b
Gersha <joonas.puua@eesti.ee>
2012-05-24 01:18:23 +0300

    ei tea mis aga midagi on

--- Service/telBookService/Client/addcontact_page.xaml.cs
+++ Service/telBookService/Client/addcontact_page.xaml.cs
@@ -21,6 +21,7 @@ namespace Client
     public partial class addcontact_page : Page
     {
         ItelBookServiceClient klient;
+        private int id = -1;
         public addcontact_page()
         {
             InitializeComponent();
@@ -41,19 +42,29 @@ namespace Client
             kontakt.Skype = skype.Text;
             kontakt.Tel = telefon.Text;
             kontakt.Userfk = adder.Id;
-            
+            if (id >= 0)
+            {
+
+                kontakt.Id = id;
+
+            }
             klient.saveContact(kontakt);
+
+
+                id = -1;
+
         }
 
         public void editContact(Contact kontakt)
         {
-            add.Content = "Edit";
+            add.Content = "Save";
             aadress.Text = kontakt.Address;
             email.Text =  kontakt.Email;
             eesnimi.Text =kontakt.FirstName;
             perenimi.Text =kontakt.LastName;
             skype.Text =kontakt.Skype;
-            telefon.Text =  kontakt.Tel;
+            telefon.Text = kontakt.Tel;
+            id = kontakt.Id;
         }
     }
 }
